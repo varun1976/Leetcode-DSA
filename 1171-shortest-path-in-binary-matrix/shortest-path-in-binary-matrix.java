72590f98036b77f5818e5422a9ba6f4a85d478ee
VARUN KONDREDDY <kondreddyvarunkumar@gmail.com>
2026-08-29 13:40:15 +0530

    Time: 16 ms (55.20%) | Memory: 47.4 MB (86.45%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -0,0 +1,38 @@
+class Pair{
+    int row, col, dist;
+    Pair(int row,int col,int dist){
+        this.row=row;
+        this.col=col;
+        this.dist=dist;
+    }
+}
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int n=grid.length;
+        if(grid[0][0]==1 || grid[n-1][n-1]==1) return -1;
+        int mini=(int)1e6;
+        int dr[]={-1,-1,0,1,1,1,0,-1};
+        int dc[]={0,1,1,1,0,-1,-1,-1};
+        boolean vis[][]=new boolean[n][n];
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(0,0,0));
+        vis[0][0]=true;
+        while(!q.isEmpty()){
+            int currRow=q.peek().row;
+            int currCol=q.peek().col;
+            int currDist=q.peek().dist;
+            q.poll();
+            if(currRow==n-1 && currCol==n-1) mini=Math.min(mini,currDist);
+            for(int i=0;i<8;i++){
+                int adjRow=currRow+dr[i];
+                int adjCol=currCol+dc[i];
+                if(adjRow>=0 && adjRow<n && adjCol>=0 && adjCol<n && !vis[adjRow][adjCol] && grid[adjRow][adjCol]==0){
+                    q.add(new Pair(adjRow,adjCol,currDist+1));
+                    vis[adjRow][adjCol]=true;
+                }
+            }
+        }
+        if(mini==1e6) return -1;
+        return mini+1;
+    }
+}
