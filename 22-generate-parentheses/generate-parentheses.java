3b87d4ba244972edd9877cf8fcef024cd0cd4b6e
VARUN KONDREDDY <kondreddyvarunkumar@gmail.com>
2026-09-07 17:46:58 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (86.69%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, new StringBuilder(), 0, 0, n);
+        return result;
+    }
+
+    private void backtrack(List<String> result, StringBuilder current, int open, int close, int max) {
+        // Base case: string length is 2 * n
+        if (current.length() == max * 2) {
+            result.add(current.toString());
+            return;
+        }
+
+        // Can we add an open parenthesis?
+        if (open < max) {
+            current.append('(');
+            backtrack(result, current, open + 1, close, max);
+            current.deleteCharAt(current.length() - 1); // backtrack
+        }
+
+        // Can we add a close parenthesis?
+        if (close < open) {
+            current.append(')');
+            backtrack(result, current, open, close + 1, max);
+            current.deleteCharAt(current.length() - 1); // backtrack
+        }
+    }
+}
