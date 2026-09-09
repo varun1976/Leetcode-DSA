a334ccb37e8b3be0c01bd21c7a01afac43c977ab
VARUN KONDREDDY <kondreddyvarunkumar@gmail.com>
2026-09-09 18:39:51 +0530

    Time: 1 ms (99.36%) | Memory: 42.9 MB (11.54%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long countCommas(long n) {
+        if(n<1000) return 0;
+        long count=0;
+        for(long i=1000;i<=n;i*=1000){
+            count+=n-i+1;
+        }
+        return count;
+    }
+}
