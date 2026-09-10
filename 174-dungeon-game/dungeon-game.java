2ed9a3f64c91394f676b73ab2dd2f7547a036023
VARUN KONDREDDY <kondreddyvarunkumar@gmail.com>
2026-09-10 15:44:23 +0530

    Time: 3 ms (29.30%) | Memory: 45.5 MB (10.19%) - LeetSync

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -16,6 +16,22 @@ class Solution {
         m=dungeon.length;
         n=dungeon[0].length;
         Integer dp[][]=new Integer[m][n];
-        return f(0,0,dungeon,dp);
+        dp[m-1][n-1]=Math.max(1,1-dungeon[m-1][n-1]);
+
+        for(int j=n-2;j>=0;j--)
+            dp[m-1][j]=Math.max(1,dp[m-1][j+1]-dungeon[m-1][j]);
+
+        for(int i=m-2;i>=0;i--)
+            dp[i][n-1]=Math.max(1,dp[i+1][n-1]-dungeon[i][n-1]);
+
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                int down=dp[i+1][j];
+                int right=dp[i][j+1];
+                dp[i][j]=Math.max(1,Math.min(down,right)-dungeon[i][j]);
+            }
+        }
+
+        return dp[0][0];
     }
 }
