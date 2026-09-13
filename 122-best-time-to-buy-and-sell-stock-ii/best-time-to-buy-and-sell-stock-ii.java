740573f0293e87092775c3e8f222563ef4435233
VARUN KONDREDDY <kondreddyvarunkumar@gmail.com>
2026-09-13 18:23:44 +0530

    Time: 1 ms (19.09%) | Memory: 46.4 MB (66.73%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,27 +1,40 @@
 class Solution {
-    public int f(int ind,int canBuy,int prices[],int n,Integer dp[][]){
-        if(ind==n-1){
-            if(canBuy==0) return prices[ind];
-            return 0;
-        }
-        if(dp[ind][canBuy]!=null) return dp[ind][canBuy];
-        int profit=0;
+    public static int f(int i,int canBuy,int prices[],int n,int dp[][]){
+        if(i==n) return 0;
+        if(dp[i][canBuy]!=-1) return dp[i][canBuy];
+        int profit;
         if(canBuy==1){
-           int buy= -prices[ind]+f(ind+1,0,prices,n,dp);
-           int dontBuy= 0+f(ind+1,1,prices,n,dp);
-           profit=Math.max(buy,dontBuy);
+            int buy=-prices[i]+f(i+1,0,prices,n,dp);
+            int dontBuy=0+f(i+1,1,prices,n,dp);
+            profit=Math.max(buy,dontBuy);
         }
         else{
-            int sell=prices[ind]+f(ind+1,1,prices,n,dp);
-            int dontSell=0+f(ind+1,0,prices,n,dp);
+            int sell=prices[i]+f(i+1,1,prices,n,dp);
+            int dontSell=0+f(i+1,0,prices,n,dp);
             profit=Math.max(sell,dontSell);
         }
-
-        return dp[ind][canBuy]=profit;
+        return dp[i][canBuy]=profit;
     }
     public int maxProfit(int[] prices) {
         int n=prices.length;
-        Integer dp[][]=new Integer[n][2];
-        return f(0,1,prices,n,dp);
+        int ahead[]=new int[2];
+        int curr[]=new int[2];
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                if(j==1){
+                    int buy=-prices[i]+ahead[0];
+                    int dontBuy=0+ahead[1];
+                    curr[j]=Math.max(buy,dontBuy);
+                }
+                else{
+                    int sell=prices[i]+ahead[1];
+                    int dontSell=0+ahead[0];
+                    curr[j]=Math.max(sell,dontSell);
+                }
+            }
+            ahead=curr;
+        }
+
+        return ahead[1];
     }
 }
