c6117bba39c5baa1168b43a5f71f153009f9c050
VARUN KONDREDDY <kondreddyvarunkumar@gmail.com>
2026-09-13 18:07:36 +0530

    Time: 1 ms (99.93%) | Memory: 46.1 MB (28.30%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,13 +1,17 @@
 class Solution {
-    public int find(int n,int i,int j,List<List<Integer>> triangle,int dp[][]){
+    public int f(int n,int i,int j,List<List<Integer>> triangle,Integer dp[][]){
         if(i==n-1) return triangle.get(i).get(j);
-        if(dp[i][j]!=-1) return dp[i][j];
-        int down=triangle.get(i).get(j)+find(n,i+1,j,triangle,dp);
-        int dia=triangle.get(i).get(j)+find(n,i+1,j+1,triangle,dp);
-
+        if(dp[i][j]!=null) return dp[i][j];
+        int down=triangle.get(i).get(j)+f(n,i+1,j,triangle,dp);
+        int dia=triangle.get(i).get(j)+f(n,i+1,j+1,triangle,dp);
         return dp[i][j]=Math.min(down,dia);
     }
     public int minimumTotal(List<List<Integer>> triangle) {
+        int n=triangle.size();
+        Integer dp[][]=new Integer[n][n];
+        return f(n,0,0,triangle,dp);
+    }
+    public int minimumTotalTabulation(List<List<Integer>> triangle) {
         int n=triangle.size();
         int prev[]=new int[n];
         for(int j=0;j<n;j++){
