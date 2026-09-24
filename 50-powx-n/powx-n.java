6f54d5e2dd96d0814960f092f477c5dda71b989a
VARUN KONDREDDY <kondreddyvarunkumar@gmail.com>
2026-09-24 22:23:56 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (70.01%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,14 @@
+class Solution {
+    public double help(double x,long n){
+        if(n==0) return 1;
+        if(n==1) return x;
+        if(n==2) return x*x;
+        double half=help(x,n/2);
+        return half*half*help(x,n%2);
+    }
+    public double myPow(double x, int n) {
+        return Math.pow(x,n);
+        // if(n>0) return help(x,n);
+        //  return 1/help(x,-(long)n);
+    }
+}
